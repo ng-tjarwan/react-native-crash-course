ed9203d0120f400c84be10341df7b4435056ac70
ng-tjarwan <tareq.jarwan@bitoasis.net>
2024-05-21 17:26:42 +0300

    implement onboarding screen

---
+++ components/CustomButton.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+interface CustomButtonProps {
+  title: string;
+  handlePress: () => void;
+  containerStyles: string;
+  textStyles: string;
+  isLoading: boolean;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading = false,
+}) => {
+  return (
+    <TouchableOpacity
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading && "opacity-50"
+      }`}
+      onPress={handlePress}
+      activeOpacity={0.7}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
+
+const styles = StyleSheet.create({});
